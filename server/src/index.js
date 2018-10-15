ad8aba7ca3d78dbb57c5e81b78765385abdd5aff
LWY <gashzack@hotmail.com>
2018-10-16 01:46:51 +0900

    Update server setup code with test POST route

--- server/src/index.js
+++ server/src/index.js
@@ -1,7 +1,37 @@
 const express = require("express");
+const bodyParser = require("body-parser");
+
 const app = express();
 const port = 3000;
 
+// ==================================================
+// middleware
+// ==================================================
+
+// Required to parse form and json data in req.body
+app.use(bodyParser.urlencoded({ extended: false }));
+app.use(bodyParser.json());
+
+// ==================================================
+// paths
+// ==================================================
+
 app.get("/", (req, res) => res.send("Hello World!"));
+
+app.post("/foo", (req, res) => {
+  console.log("###", req.body);
+  console.log("###", req.params);
+  console.log("###", req.query);
+
+  const { myVal } = req.body;
+
+  res.status(200).json({
+    returnVal: myVal
+  });
+});
+
+// ==================================================
+// entry
+// ==================================================
 
 app.listen(port, () => console.log(`Example app listening on port ${port}!`));
