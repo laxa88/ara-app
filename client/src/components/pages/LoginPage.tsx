439e74cf27f5088a5d47d2bf7b45bdbb163596c9
LWY <gashzack@hotmail.com>
2018-10-26 01:28:00 +0900

    Update LoginPage component

--- client/src/components/pages/LoginPage.tsx
+++ client/src/components/pages/LoginPage.tsx
@@ -1,19 +1,52 @@
 import * as React from 'react';
 
-import Input from '../elements/Input';
+import InputForm from '../elements/InputForm';
 
-class LoginPage extends React.Component<{}, {}> {
+interface IState {
+  email: string;
+  password: string;
+}
+
+class LoginPage extends React.Component<{}, IState> {
   constructor(props: {}) {
     super(props);
+
+    this.onChangeEmail = this.onChangeEmail.bind(this);
+    this.onChangePassword = this.onChangePassword.bind(this);
+
+    this.state = {
+      email: '',
+      password: '',
+    };
   }
 
   public render() {
     return (
       <div>
-        <Input isActive={true} type="email" placeholder="Input email here" />
-        <Input isActive={false} placeholder="Input password here" />
+        <InputForm
+          label="Email"
+          type="email"
+          value={this.state.email}
+          placeholder="Input email here"
+          onChange={this.onChangeEmail}
+        />
+        <InputForm
+          label="Password"
+          value={this.state.password}
+          type="password"
+          placeholder="Input password here"
+          onChange={this.onChangePassword}
+        />
       </div>
     );
+  }
+
+  private onChangeEmail(value: string, prevValue: string) {
+    this.setState({ email: value });
+  }
+
+  private onChangePassword(value: string, prevValue: string) {
+    this.setState({ password: value });
   }
 }
 
