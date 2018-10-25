1f09b88af46afb07726eb877d0a8cc37d63e4d00
LWY <gashzack@hotmail.com>
2018-10-26 01:27:38 +0900

    Refactor Input component

--- client/src/components/elements/Input.tsx
+++ client/src/components/elements/Input.tsx
@@ -1,94 +1,47 @@
 import * as React from 'react';
 import styled from '../css/styled';
 
-interface IInput {
-  isActive: boolean;
-}
-
-interface IInput2 {
-  primary: boolean;
-}
-
-const Input = styled<IInput, 'input'>('input')`
+const StyledInput = styled.input`
   font-size: 1em;
   margin: 1em;
   padding: 0.25em 1em;
   border-radius: 3px;
-
-  /* Color the border and text with theme.main */
-  color: ${props => props.theme.primaryColor};
-  border: 2px solid
-    ${props =>
-      props.isActive
-        ? props.theme.primaryColor
-        : props.theme.primaryColorInverted};
 `;
 
-const Input2 = styled<IInput2, 'input'>('input')`
-  font-size: 1em;
-  margin: 1em;
-  padding: 0.25em 1em;
-  border-radius: 3px;
-
-  /* Color the border and text with theme.main */
-  color: ${props => props.theme.primaryColorInverted};
-  border: 2px solid ${props => (props.primary ? 'blue' : 'yellow')};
-`;
-
-Input.defaultProps = {
-  theme: {
-    primaryColor: 'palevioletred',
-    primaryColorInverted: 'green',
-  },
-};
-
-Input2.defaultProps = {
-  theme: {
-    primaryColor: 'palevioletred',
-    primaryColorInverted: 'green',
-  },
-};
-
-interface IProps {
-  isActive: boolean;
-  placeholder: string;
-  type?: string;
-}
-
-interface IState {
+export interface IInput {
   value: string;
+  placeholder?: string;
+  type?: string;
+  onChange: (newValue: string, prevValue: string) => void;
 }
 
-class InputBase extends React.Component<IProps, IState> {
-  constructor(props: IProps) {
+class Input extends React.Component<IInput, {}> {
+  constructor(props: IInput) {
     super(props);
 
     this.onChange = this.onChange.bind(this);
-
-    this.state = {
-      value: '',
-    };
   }
 
   public onChange(e: React.ChangeEvent<HTMLInputElement>) {
-    this.setState({ value: e.currentTarget.value });
+    const { value: newValue } = e.currentTarget;
+    const { value: prevValue } = this.props;
+    this.props.onChange(newValue, prevValue);
   }
 
   public render() {
-    const { placeholder, ...others } = this.props;
+    const { value, placeholder, onChange, ...others } = this.props;
 
     return (
       <div>
-        <Input
+        <StyledInput
+          value={value}
           placeholder={placeholder}
-          value={this.state.value}
           onChange={this.onChange}
           {...others}
         />
-        <Input2 primary={this.props.isActive} />
       </div>
     );
   }
 }
 
-export default InputBase;
+export default Input;
