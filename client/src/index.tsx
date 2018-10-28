b25c2f2d16284761595d1974995ad395906680d9
LWY <gashzack@hotmail.com>
2018-10-28 13:40:12 +0900

    Update entry index.tsx to use AppRouter

--- client/src/index.tsx
+++ client/src/index.tsx
@@ -2,12 +2,12 @@ import * as React from 'react';
 import { render } from 'react-dom';
 import { Provider } from 'react-redux';
 
-import { LoginPage } from './containers';
+import AppRouter from './components/AppRouter';
 import { store } from './store';
 
 render(
   <Provider store={store}>
-    <LoginPage />
+    <AppRouter />
   </Provider>,
   document.getElementById('app'),
 );
