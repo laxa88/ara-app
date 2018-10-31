516101ea4974e451441aecfd5a89e6d149da554d
LWY <gashzack@hotmail.com>
2018-11-01 02:21:58 +0900

    Move AppRouter to routes folder

--- client/src/index.tsx
+++ client/src/index.tsx
@@ -2,7 +2,7 @@ import * as React from 'react';
 import { render } from 'react-dom';
 import { Provider } from 'react-redux';
 
-import AppRouter from './components/AppRouter';
+import { AppRouter } from './routes';
 import { store } from './store';
 
 render(
