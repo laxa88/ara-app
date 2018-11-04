a5b64a3ea7281ad05ab89cda6769f0a44b2899d7
LWY <gashzack@hotmail.com>
2018-11-04 22:11:06 +0900

    Add pooled db connection helper

---
+++ server/src/helpers/conn.ts
@@ -0,0 +1,23 @@
+import pg from "pg";
+import config from "../../../config";
+
+const pool = new pg.Pool({
+  database: config.db,
+  host: config.host,
+  password: config.pass,
+  port: config.port,
+  user: config.user,
+});
+
+pool.on("error", (err, client) => {
+  console.error("Unexpected error on idle client", err);
+  process.exit(-1);
+});
+
+const conn = async (logic: (client: pg.PoolClient) => any) => {
+  const client = await pool.connect();
+  await logic(client);
+  client.release();
+};
+
+export default conn;
