2da209a24658f2d0120383873e22ef0e757f66c7
LWY <gashzack@hotmail.com>
2018-11-07 00:12:41 +0900

    Add payment controller

--- server/src/controllers/index.ts
+++ server/src/controllers/index.ts
@@ -1,5 +1,6 @@
 import * as auth from "./auth";
 import * as house from "./house";
+import * as payment from "./payment";
 import * as user from "./user";
 
-export default { auth, house, user };
+export default { auth, house, payment, user };
---
+++ server/src/controllers/payment.ts
@@ -0,0 +1,50 @@
+import Express from "express";
+import pg from "pg";
+import SQL from "sql-template-strings";
+import conn from "../helpers/conn";
+import { parseToken } from "../helpers/token";
+import { IUser, UserType } from "../models/user";
+
+const getPayments = async (req: Express.Request, res: Express.Response) => {
+  const userData: IUser = parseToken(req.headers.authorization);
+
+  const logic = async (pc: pg.PoolClient) => {
+    const result = await pc.query(
+      SQL`
+        SELECT * FROM payments
+        WHERE user_id = ${userData.id}
+        ORDER BY id DESC
+      `,
+    );
+
+    res.status(200).json(result.rows);
+  };
+
+  await conn(logic);
+};
+
+const addPayment = async (req: Express.Request, res: Express.Response) => {
+  const logic = async (pc: pg.PoolClient) => {
+    // TODO
+  };
+
+  await conn(logic);
+};
+
+const updatePayment = async (req: Express.Request, res: Express.Response) => {
+  const logic = async (pc: pg.PoolClient) => {
+    // TODO
+  };
+
+  await conn(logic);
+};
+
+const approvePayment = async (req: Express.Request, res: Express.Response) => {
+  const logic = async (pc: pg.PoolClient) => {
+    // TODO
+  };
+
+  await conn(logic);
+};
+
+export { getPayments, addPayment, updatePayment, approvePayment };
