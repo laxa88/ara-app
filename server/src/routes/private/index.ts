f5138956900c6fdc6baa74ac94a2a35318eb3da8
LWY <gashzack@hotmail.com>
2018-11-07 00:13:01 +0900

    Add payment routes

--- server/src/routes/private/index.ts
+++ server/src/routes/private/index.ts
@@ -13,4 +13,9 @@ router.put("/v1/users", controllers.user.updateUser);
 router.get("/v1/houses", controllers.house.getHouses);
 router.put("/v1/houses/:id", controllers.house.updateHouse);
 
+router.get("/v1/payments", controllers.payment.getPayments);
+router.post("/v1/payments", controllers.payment.addPayment);
+router.put("/v1/payments/:id", controllers.payment.updatePayment);
+router.put("/v1/payments/:id/approve", controllers.payment.approvePayment);
+
 export default router;
