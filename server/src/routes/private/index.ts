26e1a83be5ae1e227238f520317b8fe89ceb7c18
LWY <gashzack@hotmail.com>
2018-11-06 19:35:53 +0900

    Add updateHouse route

--- server/src/routes/private/index.ts
+++ server/src/routes/private/index.ts
@@ -7,5 +7,6 @@ const router = express.Router();
 router.use(authenticate);
 router.get("/v1/users", controllers.user.getUsers);
 router.get("/v1/houses", controllers.house.getHouses);
+router.post("/v1/houses/:id", controllers.house.updateHouse);
 
 export default router;
