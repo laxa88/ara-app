2b08317444064fde805d657552c00514a07b9aa3
LWY <gashzack@hotmail.com>
2018-11-06 23:37:47 +0900

    Add addUser and updateUser routes

--- server/src/routes/private/index.ts
+++ server/src/routes/private/index.ts
@@ -5,7 +5,11 @@ import authenticate from "../../middleware/auth";
 const router = express.Router();
 
 router.use(authenticate);
+
 router.get("/v1/users", controllers.user.getUsers);
+router.post("/v1/users", controllers.user.addUser);
+router.put("/v1/users", controllers.user.updateUser);
+
 router.get("/v1/houses", controllers.house.getHouses);
 router.post("/v1/houses/:id", controllers.house.updateHouse);
 
