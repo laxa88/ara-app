a3d6ef8d0dd6ebb1a84ac0ffefbc32e74588b39d
LWY <gashzack@hotmail.com>
2018-11-08 02:53:53 +0900

    Update deleteAttachment logic

--- server/src/controllers/attachment.ts
+++ server/src/controllers/attachment.ts
@@ -33,6 +33,8 @@ const addAttachment = async (req: Express.Request, res: Express.Response) => {
       return;
     }
 
+    // TODO upload file logic goes here
+
     const result2 = await pc.query(
       SQL`
         INSERT INTO attachments (payment_id, file_name)
@@ -58,8 +60,43 @@ const deleteAttachment = async (
   req: Express.Request,
   res: Express.Response,
 ) => {
+  const { id } = req.params;
+
+  const userData: IUser = parseToken(req.headers.authorization);
+
   const logic = async (pc: pg.PoolClient) => {
-    // TODO
+    const result1 = await pc.query(
+      SQL`
+        SELECT *
+        FROM attachments
+        INNER JOIN payments
+        ON attachments.payment_id = payments.id
+        INNER JOIN users
+        ON payments.user_id = users.id
+        AND attachments.id = ${id}
+        AND users.id = ${userData.id}
+      `,
+    );
+
+    if (!result1.rowCount) {
+      res.status(400).json({ message: "Invalid user or attachment ID." });
+      return;
+    }
+
+    // TODO delete file logic goes here
+
+    const result2 = await pc.query(
+      SQL`
+        DELETE FROM attachments
+        WHERE id = ${id}
+      `,
+    );
+
+    if (result2.rowCount) {
+      res.status(200).json({ message: "Success." });
+    } else {
+      res.status(400).json({ message: "Unable to add attachment." });
+    }
   };
 
   try {
