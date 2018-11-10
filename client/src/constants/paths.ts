107ddfb5b6c9a88a763a28128fdf2bfa33c36d52
LWY <gashzack@hotmail.com>
2018-11-10 19:27:09 +0900

    Add base API path in constants

--- client/src/constants/paths.ts
+++ client/src/constants/paths.ts
@@ -1,9 +1,11 @@
 const development = {
+  BASE_API_PATH: 'http://localhost:3000/api',
   BASE_CLIENT_PATH: 'http://localhost:3001',
   BASE_SERVER_PATH: 'http://localhost:3000',
 };
 
 const production = {
+  BASE_API_PATH: 'http://localhost:3000/api',
   BASE_CLIENT_PATH: 'http://localhost:3001',
   BASE_SERVER_PATH: 'http://localhost:3000',
 };
