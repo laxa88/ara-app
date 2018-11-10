6046aa85ebd9fade9133760db860f49212233502
LWY <gashzack@hotmail.com>
2018-11-11 02:08:13 +0900

    Add saga middleware init logic

--- client/src/store/index.ts
+++ client/src/store/index.ts
@@ -1,11 +1,20 @@
-import { combineReducers, createStore } from 'redux';
+import { applyMiddleware, combineReducers, createStore } from 'redux';
 import { composeWithDevTools } from 'redux-devtools-extension';
+import reduxSaga from 'redux-saga';
 import { loginPage } from './LoginPage/reducer';
+import { saga as loginPageSaga } from './LoginPage/sagas';
 
 const reducers = combineReducers({
   loginPage,
 });
 
-const store = createStore(reducers, composeWithDevTools());
+const sagaMiddleware = reduxSaga();
+
+const store = createStore(
+  reducers,
+  composeWithDevTools(applyMiddleware(sagaMiddleware)),
+);
+
+sagaMiddleware.run(loginPageSaga);
 
 export { store };
