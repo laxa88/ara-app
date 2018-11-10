44f3fb72157fc503d1241635a2699b974d8b4d02
LWY <gashzack@hotmail.com>
2018-11-10 18:53:10 +0900

    Add cors middleware logic

---
+++ server/src/middleware/cors.ts
@@ -0,0 +1,19 @@
+import cors, { CorsOptions } from "cors";
+
+// Reference: https://medium.com/@alexishevia/using-cors-in-express-cac7e29b005b
+const corsConfig: CorsOptions = {
+  origin: (origin, cb) => {
+    // Allow requests with no origin, e.g. Postman, Insomnia, etc.
+    if (!origin) {
+      return cb(null, true);
+    }
+
+    const allowedOrigins = ["http://localhost:3001"];
+
+    if (allowedOrigins.indexOf(origin) === -1) {
+      return cb(new Error("CORS violation: Invalid origin."), false);
+    }
+  },
+};
+
+export default cors(corsConfig);
