74e799ffac05b1b38c9fa3de8715a3ab0a0ed23d
LWY <gashzack@hotmail.com>
2018-11-10 18:53:52 +0900

    Add base path constants

---
+++ client/src/constants/paths.ts
@@ -0,0 +1,11 @@
+const development = {
+  BASE_CLIENT_PATH: 'http://localhost:3001',
+  BASE_SERVER_PATH: 'http://localhost:3000',
+};
+
+const production = {
+  BASE_CLIENT_PATH: 'http://localhost:3001',
+  BASE_SERVER_PATH: 'http://localhost:3000',
+};
+
+export { development, production };
