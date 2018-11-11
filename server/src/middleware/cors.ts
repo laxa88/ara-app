af435aebd7d8d71a2f43e1485ff19581bc9f2032
LWY <gashzack@hotmail.com>
2018-11-12 00:20:32 +0800

    Update server cors middleware to use config port

--- server/src/middleware/cors.ts
+++ server/src/middleware/cors.ts
@@ -1,14 +1,17 @@
 import cors, { CorsOptions } from "cors";
+import config from "../../../config";
 
 // Reference: https://medium.com/@alexishevia/using-cors-in-express-cac7e29b005b
 const corsConfig: CorsOptions = {
   origin: (origin, cb) => {
+    const { clientPort } = config;
+
     // Allow requests with no origin, e.g. Postman, Insomnia, etc.
     if (!origin) {
       return cb(null, true);
     }
 
-    const allowedOrigins = ["http://localhost:3001"];
+    const allowedOrigins = [`http://localhost:${clientPort}`];
 
     if (allowedOrigins.indexOf(origin) === -1) {
       return cb(new Error("CORS violation: Invalid origin."), false);
