61f2a1e8a7eaaae0611d35b66c7036282b0ee30d
LWY <gashzack@hotmail.com>
2018-11-14 20:00:43 +0800

    Update store reducer definition

--- client/src/store/types.ts
+++ client/src/store/types.ts
@@ -1,5 +1,7 @@
+import { IState as authState } from './Auth/types';
 import { IState as loginPageState } from './LoginPage/types';
 
 export interface IReducers {
   loginPage: loginPageState;
+  auth: authState;
 }
