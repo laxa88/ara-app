103024070f15509459095a5785d7da46e2aa5a5e
LWY <gashzack@hotmail.com>
2018-11-14 17:51:27 +0800

    Fix client path constants

--- client/src/constants/paths.ts
+++ client/src/constants/paths.ts
@@ -1,13 +1,15 @@
+import * as config from '../../../config';
+
 const development = {
-  BASE_API_PATH: 'http://localhost:3000/api',
-  BASE_CLIENT_PATH: 'http://localhost:3001',
-  BASE_SERVER_PATH: 'http://localhost:3000',
+  BASE_API_PATH: `http://localhost:${config.serverPort}/api`,
+  BASE_CLIENT_PATH: `http://localhost:${config.clientPort}`,
+  BASE_SERVER_PATH: `http://localhost:${config.serverPort}`,
 };
 
 const production = {
-  BASE_API_PATH: 'http://localhost:3000/api',
-  BASE_CLIENT_PATH: 'http://localhost:3001',
-  BASE_SERVER_PATH: 'http://localhost:3000',
+  BASE_API_PATH: `http://localhost:${config.serverPort}/api`,
+  BASE_CLIENT_PATH: `http://localhost:${config.clientPort}`,
+  BASE_SERVER_PATH: `http://localhost:${config.serverPort}`,
 };
 
 export { development, production };
