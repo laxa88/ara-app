8d60da465d620b6a6b6c21010e8368774b68d65e
LWY <gashzack@hotmail.com>
2018-11-14 20:00:00 +0800

    Update store index with updated sagas and reducers

--- client/src/store/index.ts
+++ client/src/store/index.ts
@@ -1,10 +1,12 @@
 import { applyMiddleware, combineReducers, createStore } from 'redux';
 import { composeWithDevTools } from 'redux-devtools-extension';
 import reduxSaga from 'redux-saga';
-import { loginPage } from './LoginPage/reducer';
-import { saga as loginPageSaga } from './LoginPage/sagas';
+import { reducer as auth } from './Auth/reducer';
+import { loginSaga, logoutSaga } from './Auth/sagas';
+import { reducer as loginPage } from './LoginPage/reducer';
 
 const reducers = combineReducers({
+  auth,
   loginPage,
 });
 
@@ -15,6 +17,7 @@ const store = createStore(
   composeWithDevTools(applyMiddleware(sagaMiddleware)),
 );
 
-sagaMiddleware.run(loginPageSaga);
+sagaMiddleware.run(loginSaga);
+sagaMiddleware.run(logoutSaga);
 
 export { store };
