3175b596da09914a2ac2272a8d896c69d7ec0618
LWY <gashzack@hotmail.com>
2018-11-16 23:18:43 +0900

    Add redux-persist logic

--- client/src/index.tsx
+++ client/src/index.tsx
@@ -2,12 +2,15 @@ import * as React from 'react';
 import { render } from 'react-dom';
 import { Provider } from 'react-redux';
 
+import { PersistGate } from 'redux-persist/integration/react';
 import { AppRouter } from './routes';
-import { store } from './store';
+import { persistor, store } from './store';
 
 render(
   <Provider store={store}>
-    <AppRouter />
+    <PersistGate persistor={persistor}>
+      <AppRouter />
+    </PersistGate>
   </Provider>,
   document.getElementById('app'),
 );
--- client/src/store/index.ts
+++ client/src/store/index.ts
@@ -1,23 +1,34 @@
 import { applyMiddleware, combineReducers, createStore } from 'redux';
 import { composeWithDevTools } from 'redux-devtools-extension';
+import { persistReducer, persistStore } from 'redux-persist';
+import storage from 'redux-persist/lib/storage';
 import reduxSaga from 'redux-saga';
 import { reducer as auth } from './Auth/reducer';
 import { loginSaga, logoutSaga } from './Auth/sagas';
 import { reducer as loginPage } from './LoginPage/reducer';
+
+const persistConfig = {
+  storage,
+  key: 'root',
+};
 
 const reducers = combineReducers({
   auth,
   loginPage,
 });
 
+const persistedReducer = persistReducer(persistConfig, reducers);
+
 const sagaMiddleware = reduxSaga();
 
 const store = createStore(
-  reducers,
+  persistedReducer,
   composeWithDevTools(applyMiddleware(sagaMiddleware)),
 );
+
+const persistor = persistStore(store);
 
 sagaMiddleware.run(loginSaga);
 sagaMiddleware.run(logoutSaga);
 
-export { store };
+export { store, persistor };
