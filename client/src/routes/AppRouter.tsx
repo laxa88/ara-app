75abbc562916fc7b9905a2eb8d9d88b7fb3bfa43
LWY <gashzack@hotmail.com>
2018-11-16 23:17:55 +0900

    Rename DashboardPage to Dashboard

--- client/src/routes/AppRouter.tsx
+++ client/src/routes/AppRouter.tsx
@@ -1,13 +1,13 @@
 import * as React from 'react';
 import { BrowserRouter as Router, Link, Route } from 'react-router-dom';
-import DashboardPage from '../containers/DashboardPage';
+import Dashboard from '../containers/Dashboard';
 import LoginPage from '../containers/LoginPage';
 
 const AppRouter = () => (
   <Router>
     <div>
       <Route path="/" exact={true} component={LoginPage} />
-      <Route path="/dashboard" component={DashboardPage} />
+      <Route path="/dashboard" component={Dashboard} />
     </div>
   </Router>
 );
