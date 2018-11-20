2735a733579cc803c14476929cf92f8f190cca6b
LWY <gashzack@hotmail.com>
2018-11-20 23:42:09 +0900

    Update top-level store type definition

--- client/src/store/types.ts
+++ client/src/store/types.ts
@@ -1,7 +1,9 @@
 import { IState as IAuthState } from './Auth/types';
 import { IState as ILoginPageState } from './LoginPage/types';
+import { IState as IPaymentState } from './Payments/types';
 
 export interface IReducers {
+  payments: IPaymentState;
   loginPage: ILoginPageState;
   auth: IAuthState;
 }
