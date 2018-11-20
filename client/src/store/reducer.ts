90b9ab10a8989b0d6fd6da6af74a0eda2eacaee7
LWY <gashzack@hotmail.com>
2018-11-20 23:41:50 +0900

    Add top-level reducer and saga index files

---
+++ client/src/store/reducer.ts
@@ -0,0 +1,5 @@
+import { reducer as auth } from './Auth/reducer';
+import { reducer as loginPage } from './LoginPage/reducer';
+import { reducer as payments } from './Payments/reducer';
+
+export { auth, loginPage, payments };
---
+++ client/src/store/sagas.ts
@@ -0,0 +1,16 @@
+import { loginSaga, logoutSaga } from './Auth/sagas';
+import {
+  addPaymentSaga,
+  approvePaymentSaga,
+  getPaymentsSaga,
+  updatePaymentSaga,
+} from './Payments/sagas';
+
+export {
+  loginSaga,
+  logoutSaga,
+  addPaymentSaga,
+  approvePaymentSaga,
+  getPaymentsSaga,
+  updatePaymentSaga,
+};
