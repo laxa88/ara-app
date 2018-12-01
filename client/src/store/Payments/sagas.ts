f66cdd4abcc53e609677662a00fcb6adfa8bfaad
LWY <gashzack@hotmail.com>
2018-12-01 14:50:45 +0900

    Minor refactor Payments saga

--- client/src/store/Payments/sagas.ts
+++ client/src/store/Payments/sagas.ts
@@ -5,7 +5,7 @@ import * as service from '../../services/payments';
 import * as actions from './actions';
 import { ActionTypes, IAction } from './types';
 
-function* getPayments(action: IAction) {
+function* getPayments() {
   try {
     const response = yield call(service.getPayments);
     yield put<IAction>(actions.getPaymentsSuccess(response.data));
@@ -18,7 +18,7 @@ function* getPayments(action: IAction) {
 function* addPayment(action: IAction) {
   try {
     const { date_paid } = action.payload;
-    const response = yield call(service.addPayment, date_paid);
+    yield call(service.addPayment, date_paid);
     yield put<IAction>(actions.addPaymentSuccess());
   } catch (e) {
     const message = getValue(e, 'response.data.message') || e.toString();
@@ -29,7 +29,7 @@ function* addPayment(action: IAction) {
 function* updatePayment(action: IAction) {
   try {
     const { id, date_paid } = action.payload;
-    const response = yield call(service.updatePayment, id, date_paid);
+    yield call(service.updatePayment, id, date_paid);
     yield put<IAction>(actions.updatePaymentSuccess());
   } catch (e) {
     const message = getValue(e, 'response.data.message') || e.toString();
@@ -40,7 +40,7 @@ function* updatePayment(action: IAction) {
 function* approvePayment(action: IAction) {
   try {
     const { id, approved } = action.payload;
-    const response = yield call(service.approvePayment, id, approved);
+    yield call(service.approvePayment, id, approved);
     yield put<IAction>(actions.approvePaymentSuccess());
   } catch (e) {
     const message = getValue(e, 'response.data.message') || e.toString();
