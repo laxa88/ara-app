f6319710c29578c42aea0366c549028984f0bcbb
LWY <gashzack@hotmail.com>
2018-12-01 14:22:54 +0900

    Update payments reducer

--- client/src/store/Payments/reducer.ts
+++ client/src/store/Payments/reducer.ts
@@ -14,6 +14,7 @@ export function reducer(state: IState = defaultState, action: IAction): IState {
     case ActionTypes.APPROVE:
       return {
         ...state,
+        errorMessage: '',
         isLoading: true,
       };
 
@@ -40,6 +41,7 @@ export function reducer(state: IState = defaultState, action: IAction): IState {
         ...state,
         errorMessage: action.payload.errorMessage,
         isLoading: false,
+        payments: [],
       };
 
     default:
